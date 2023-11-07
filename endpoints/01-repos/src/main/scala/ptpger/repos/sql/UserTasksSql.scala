2e295c790511e9e0a73fa63eb33b3dfc37898bed
Prince <prince777_98@mail.ru>
2023-11-07 16:22:50 +0500

    feat(#22): small fix

--- endpoints/01-repos/src/main/scala/ptpger/repos/sql/UserTasksSql.scala
+++ endpoints/01-repos/src/main/scala/ptpger/repos/sql/UserTasksSql.scala
@@ -9,7 +9,7 @@ private[repos] object UserTasksSql extends Sql[TaskId] {
   private[repos] val codec =
     (id *: UsersSql.id).to[UserTask]
   def insertBatch(userTasks: List[UserTask]): Command[userTasks.type] =
-    sql"""INSERT INTO user_tasks VALUES (${codec.values.list(userTasks)})""".command
+    sql"""INSERT INTO user_tasks VALUES ${codec.values.list(userTasks)}""".command
 
   val select: Query[TaskId, UserTask] =
     sql"""SELECT * FROM user_tasks WHERE task_id = $id""".query(codec)
