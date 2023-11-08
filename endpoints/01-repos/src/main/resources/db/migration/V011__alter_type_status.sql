e7004531814b03c4dc464ea73b67eb8587c227b7
Behruz Boltayev <b.behruz@it-forelead.uz>
2023-11-08 23:00:40 +0500

    fix(sql-migrations): Fixed sql migration related with new 'expired' value

--- endpoints/01-repos/src/main/resources/db/migration/V011__alter_type_status.sql
+++ endpoints/01-repos/src/main/resources/db/migration/V011__alter_type_status.sql
@@ -1,6 +1,1 @@
-DO $$
-BEGIN
-    IF 'expired' NOT IN (SELECT unnest(enum_range(NULL::STATUS))) THEN
-        ALTER TYPE STATUS ADD VALUE 'expired';
-    END IF;
-END $$;
+ALTER TYPE STATUS ADD VALUE 'expired';
